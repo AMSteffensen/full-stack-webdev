9afd754ce3bafb649e21bc1149c662b9be776e31
Andreas Steffensen <andreas.steffensen89@gmail.com>
2022-07-03 18:45:14 +0000

    add todo items

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,21 @@
-<h1>Hello!!!!</h1>
+<script>
+    import TodoItem from "$lib/todo-item.svelte";
+
+    const title = 'Todos | New';
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+
+<form action="" method="" >
+     <input type="text" name="text" aria-label="Add a todo" placeholder="+ to add a new todo">
+</form>
+
+<TodoItem />
+
+<TodoItem />
+
+<TodoItem />
